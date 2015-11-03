8b6eafc8b47ce7cc54adf2070427531834499453
zhichao.zheng <johnson.zheng@dianping.com>
2015-11-03 10:14:57 +0800

    fix

---
+++ src/main/java/com/dingmei/spring/security/UserInfo.java
@@ -0,0 +1,21 @@
+package com.dingmei.spring.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+/**
+ * Created by ying on 15/11/2.
+ * 扩展User类
+ */
+public class UserInfo extends User {
+
+    public UserInfo(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, authorities);
+    }
+
+    public UserInfo(String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+    }
+}
